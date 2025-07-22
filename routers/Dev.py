5549df6347c36447cca91c99cd8fcacfbb776084
luciana-cpu <lucianasueldo1@gmail.com>
2025-07-22 08:52:30 -0300

    funciana

--- routers/Dev.py
+++ routers/Dev.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-import models
+from models.Prestamo import Prestamo
+from models.libro import Libro
 from auth.jwt import get_db, get_current_usuario
 import schemas
 
@@ -10,10 +11,10 @@ router = APIRouter(prefix= "/devolver", tags=["devolver"])
 
 @router.post("/devolver/{id}")
 def devolver_libro(id: int, db: Session = Depends(get_db), usuario=Depends(get_current_usuario)):
-    prestamo = db.query(models.Prestamo).filter_by(id==id).first()
+    prestamo = db.query(Prestamo).filter_by(id=id).first()
     if not prestamo or prestamo.usuario_id != usuario.id:
         raise HTTPException(status_code=404, detail="Préstamo no encontrado")
-    libro = db.query(models.Libro).get(prestamo.libro_id)
+    libro = db.query(Libro).get(prestamo.libro_id)
     libro.disponible = True
     db.commit()
     db.refresh(libro)
